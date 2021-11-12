faab61196051a4aa0168c71fce2e618dcb65c6f1
Deepak Vankadaru <deepak.vankadaru@gmail.com>
2021-11-11 22:23:39 -0800

    Bad commit with unit test failure

--- buzz/generator.py
+++ buzz/generator.py
@@ -17,7 +17,7 @@ def sample(l, n = 1):
 def generate_buzz():
   buzz_terms = sample(buzz, 2)
   phrase = ' '.join([sample(adjectives), buzz_terms[0], sample(adverbs),
-    sample(verbs), buzz_terms[1], "v2"])
+    sample(verbs), buzz_terms[1], "v3"])
   return phrase.title()
 
 if __name__ == '__main__':
